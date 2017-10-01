4a2a14cf3bbb644315b7ae9cd30cdb78aa9673ed
Bram <bramvereertbrugghen@live.be>
2017-10-01 18:08:31 +0200

    Added base implementation of blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,105 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Create a new Block and add to chain
-        pass
+        # Create the 'genesis' block
+        self. new_block(previous_hash=1, proof=100)
 
-    def new_transaction(self):
-        # Add new transaction to list of transactions
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Creates a new Block in the Blockchain
+
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :pram previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the sender
+        :param recipient <str> Address of the recipient
+        :param amount: <int> Amount
+
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+        - Find number 'p' such that hash(pp') contains leading 4 zeroes,
+            where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while not self.valid_proof(last_proof, proof):
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof:
+            Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous proof
+        :param proof: <int> Current proof
+        :return: <bool>
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
 
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a block
+
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered,
+        # or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        # Returns last block in the chain
-        pass
+        return self.chain[-1]
